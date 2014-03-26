e545987465532521a74d65c5d4767c0b37c5c208
luis <luis>
2014-03-26 12:58:37 +0000

    Se ha modificado el algoritmo de gauss.c para que trabaje con pivotes. De este modo cuando el rango de la matriz no es maximo, aun se pueden sacar ceros y dejarla de modo que se pueda resolver el sistema si este fuera compatible indeterminado.

--- gauss.c
+++ gauss.c
@@ -1,4 +1,4 @@
-/* $Id: gauss.c,v 1.7 2014/03/19 17:41:32 luis Exp $
+/* $Id: gauss.c,v 1.8 2014/03/26 12:58:37 luis Exp $
  * vim: ts=4 sw=4 nowrap
  * Author: Luis Colorado <lc@luiscoloradosistemas.com>
  * Date: Mon Feb 17 19:51:53 CET 2014
@@ -14,6 +14,11 @@
 #include <assert.h>
 #include <limits.h>
 #include <math.h>
+
+#ifndef FALSE
+#define	FALSE	0
+#define TRUE	(!FALSE)
+#endif
 
 typedef double **matriz;
 
@@ -135,24 +140,24 @@ double gauss(matriz A, int lin, int col, double eps, int debug)
 
 	/* vamos desde la primera fila hasta la
 	 * penultima, haciendo ceros en las filas
-	 * de debajo de la que estamos
-	 * considerando */
-	for(i = 0; i < lin-1; i++) {
-		int mod = 0;
-		if (1 || fabs(A[i][i]) < eps) {
-			/* si el elemento de la diagonal es
+	 * de debajo del pivote (el pivote es el
+	 * elemento de la fila i, columna j. */
+	for(i = 0, j = 0; i < lin && j < col; i++, j++) {
+		int mod = FALSE;
+		if (fabs(A[i][j]) < eps) {
+			/* El pivote tiene que ser distinto de
 			 * cero, no podemos hacer ceros
 			 * (hariamos una division por cero)
-			 * asi que podemos buscar el elemento
+			 * debajo de la diagonal.
+			 * Asi que buscamos el elemento
 			 * de mayor valor absoluto, debajo del
-			 * elemento de la diagonal e
-			 * intercambiar las filas. */
-			double max = fabs(A[i][i]);
+			 * pivote e intercambiamos filas. */
+			double max = fabs(A[i][j]);
 			int i_max = i;
-			for (j = i + 1; j < lin; j++) {
-				if (fabs(A[j][i]) > max) {
-					i_max = j;
-					max = fabs(A[j][i]);
+			for (k = i + 1; k < lin; k++) {
+				if (fabs(A[k][j]) > max) {
+					i_max = k;
+					max = fabs(A[k][j]);
 				} /* if */
 			} /* for j */
 			/* si i_max es distinto de i, es que
@@ -160,52 +165,58 @@ double gauss(matriz A, int lin, int col, double eps, int debug)
 			 * diagonal principal que tiene valor
 			 * absoluto mayor que el elemento de
 			 * la diagonal, intercambiamos las
-			 * filas (aqui y en la matriz inversa)
-			 * y cambiamos de signo el
+			 * filas y cambiamos de signo el
 			 * determinante. */
 			if (i_max != i) {
 				intercambia(A, col, i, i_max);
 				det = -det;
-				mod = 1;
+				mod = TRUE;
 			}
 		} /* if */
-		if (fabs(A[i][i]) < eps) {
-			printf("La matriz A es singular:\n");
-			printf("el rango es %d\n", i);
-			return 0.0;
+
+		/* vamos calculando el determinante siempre
+		 * que los pivotes esten en la diagonal. */
+		if (i == j) det *= A[i][j];
+
+		/* si el candidato a pivote sigue siendo 
+		 * demasiado peque~no, no podemos hacer
+		 * ceros, ya que dividimos por un numero
+		 * demasiado peque~no. Esto significa que
+		 * todos los elementos debajo del pivote son
+		 * cero y que no hay pivote, hemos de pasar
+		 * al elemento de la derecha. */
+		if (fabs(A[i][j]) < eps) {
+			if (i == j) {
+				/* lo imprimimos solo la primera vez. */
+				printf("La matriz A es singular.\n");
+			}
+			/* cuando se incrementen i y j, el
+			 * elemento seleccionado como pivote
+			 * sera el elemento a la derecha del
+			 * ex-pivote actual. */
+			i--; 
+			continue;
 		} /* if */
 
-		/* bien, hacemos ceros debajo de la
-		 * diagonal */
-		for (j = i + 1; j < lin; j++) {
-			if (1 || fabs(A[j][i]) >= eps) {
-				double x = -A[j][i] / A[i][i];
-				/* a la fila j, le a~nadimos)
+		/* bien, hacemos ceros debajo del
+		 * pivote */
+		for (k = i + 1; k < lin; k++) {
+			if (1 || fabs(A[k][j]) >= eps) {
+				double x = -A[k][j] / A[i][j];
+				/* a la fila k, le a~nadimos)
 				 * x por la fila i */
-				P(A, col, j, x, i);
-				mod = 1;
+				P(A, col, k, x, i);
+				mod = TRUE;
 			} /* if */
 		} /* for */
-		/* hemos hecho ceros debajo de la
-		 * diagonal */
-		/* vamos calculando el determinante */
-		det = det * A[i][i];
+		/* hemos hecho ceros debajo de los
+		 * pivotes. */
 		if (debug && mod) imprime_matriz(A, lin, col, eps);
 	} /* for i */
 
-	/* cuando llegamos aqui hemos hecho ceros
-	 * debajo de la diagonal, pero el ultimo
-	 * elemento podria ser cero.  Sacamos el
-	 * determinante. */
-	det = det * A[i][i];
-
-	if (fabs(det) < eps) {
-		printf("La matriz A es singular: det = 0.0\n");
-		printf("rango de A es %d\n", lin-1);
-		return 0.0;
-	}
-
-	if (debug) printf("El determinante vale: %lg\n", det);
+	if (debug)
+		printf("El determinante vale: %lg\n",
+			(i == j) ? det : 0.0);
 
 	/* si col > lin es que la matriz se ha introducido como
 	 * un sistema.  En este caso, debemos continuar, hacer
@@ -214,23 +225,44 @@ double gauss(matriz A, int lin, int col, double eps, int debug)
 	 * ecuaciones planteado. */
 	if (col > lin) {
 		for (i = lin-1; i > 0; i--) {
-			int mod = 0;
-			for (j = i - 1; j >= 0; j--) {
-				if (1||fabs(A[j][i]) >= eps) {
-					double x = -A[j][i] / A[i][i];
-					P(A, col, j, x, i);
-					mod = 1;
-				} /* if */
-			} /* for j */
+			int mod = FALSE;
+			double p;
+			j = i;
+			if (fabs(A[i][j]) < eps) {
+				/* buscamos el pivote.  Este sera
+				 * el primer elemento distinto de cero
+				 * (en valor absoluto, mayor que epsilon)
+				 * que haya a la derecha del elemento de
+				 * la diagonal */
+				for (j = i + 1; j < col; j++) {
+					if (fabs(A[i][j]) >= eps)
+						break;
+				}
+			} /* if */
+
+			/* si hemos encontrado el pivote... */
+			if (j < col) {
+
+				/* A[i][j] es el pivote */
+				p = A[i][j];
+				/* hacemos ceros sobre el pivote */
+				for (k = i - 1; k >= 0; k--) {
+					if (1||fabs(A[k][j]) >= eps) {
+						double x = -A[k][j] / A[i][j];
+						P(A, col, k, x, i);
+						mod = TRUE;
+					} /* if */
+				} /* for k */
+
+				/* dividimos toda la fila por el pivote. */
+				if (fabs(p - 1.0) >= eps) 
+					divide_fila(A, col, i, p);
+			} /* if */
 			if (debug && mod) imprime_matriz(A, lin, col, eps);
 		} /* for i */
+	} /* if  */
 
-		for (i = 0; i < lin; i++) {
-			double x = A[i][i];
-			divide_fila(A, col, i, x);
-		} /* for i */
-	}
 	return det;
 } /* gauss */
 
-/* $Id: gauss.c,v 1.7 2014/03/19 17:41:32 luis Exp $ */
+/* $Id: gauss.c,v 1.8 2014/03/26 12:58:37 luis Exp $ */
